1ed6295c49e6cb78fc44ad81295617bfaf0e83cc
Luca <hohmann.luca@tutanota.com>
2020-09-20 22:34:32 +0200

    Fixed export

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-//module.exports = require("./src/Youtube-Scraper")
-const ytcomments = require("./src/Youtube-Scraper")
-
-async function f(){
-    const data = await ytcomments.scrape_youtube_comments('srKTVfQt8zA')
-}
-f()
+module.exports = require("./src/Youtube-Scraper")
