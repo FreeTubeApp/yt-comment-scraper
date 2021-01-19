98f4e210fd656035e8f051a102900dd3043d61a2
Preston <freetubeapp@protonmail.com>
2021-01-19 18:31:45 -0500

    Revamp Comments Scraper

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # YouTube Comment Scraper NodeJS Documentation
-This NodeJS library scrapes the comments of the YouTube provided HTML comment data without any API usage order by date descending (so most recent first). It is developed for and tailored towards easy usage in the [FreeTube](https://github.com/FreeTubeApp/FreeTube-Vue) rewrite but can be used with any other project as well.
+This NodeJS library scrapes the comments of the YouTube provided HTML comment data without any API usage order by date descending (so most recent first). It is developed for and tailored towards easy usage in the [FreeTube](https://github.com/FreeTubeApp/FreeTube) rewrite but can be used with any other project as well.
 The library is able to scrape all comments at once or scrape only one page at a time and allowing follow-up pages to be loaded later. When performance is an issue, it is advised to use the page by page loading, because then data is only loaded when needed.
 
 Therefore, this library does not require any API keys, with the attached maximum quotas, but instead might take longer to receive the required data.
@@ -8,48 +8,65 @@ The library works as long as YouTube keeps its web page layout the same. Therefo
 If this library should not work at some point, please create an issue and let me know so that I can take a look into it. Pull requests are also welcomed in this case.
 
 ## Installation
-`npm install yt-comment-scraper`
+`npm install yt-comment-scraper --save`
 
 ##Usage
 Create a new instance of the comment scraper with optional parameters. The first one (default = true) sets the module cookie if the module has to handle the cookies itself.
 Set the value to false if you use something like Electron, which handles cookies by itself.
 The second variable changes the sorting mode of the comments from popular (default = false) to newest (true)
 ```javascript
-const CommentScraper = require("yt-comment-scraper")
-// the two boolean variable default to true and false.
-const ytcomments = new CommentScraper(setCookie = true, sortNew = false)
+const ytcm = require("yt-comment-scraper")
+
+import ytcm from 'yt-comment-scraper'
 ```
 
 ## API
-**scrape_all_youtube_comments(videoId)**
+**getAllComments(videoId, sortByNewest, setCookie)**
 
 Returns a list of objects containing **all** comments and replies of the video.
+
+- videoId (String) (Required) - The video ID to get comments from
+- sortByNewest (Boolean) (Default: false) - Whether to sort by the newest comments or to sort by the top comments.
+- setCookie (Boolean) (Default: true) - Whether to set cookies while grabbing comments or not. Sometimes needs to be `false` depending on your environment
+
+**WARNING:** This command can take a _long_ time to run depending on the amount of comments being grabbed. Be aware that this might be very resourse heavy and you may want to use `getComments()` instead to paginate through comments.
+
 ```javascript
-ytcomments.scrape_all_youtube_comments(videoId).then((data) =>{
+ytcm.getAllComments(videoId, sortByNewest, setCookie).then((data) =>{
     console.log(data);
 }).catch((error)=>{
     console.log(error);
 });
 ```
-**scrape_next_page_youtube_comments(videoId)**
+**getComments(payload)**
 
 Returns a list of objects containing comments and replies from the next page of the video.
+
+- payload (Object) (Required) - An object containing the various options
+  - videoId (String) (Required) - The video ID to grab comments from
+  - sortByNewest (Boolean) (Default: `false`) - Grabs newest comments when `true`. Grabs top comments when `false`
+  - setCookie (Boolean) (Default: `true`) - Sets a cookie internally when grabbing comments. May not be needed depending on your environment
+  - xsrf (String) (Optional) - The XSRF token needed to grab comments from a video. Module will automatically grab this token if not provided
+  - continuation (Optional) - The token from a previous call to continue grabbing comments
+
 ```javascript
-//grab the first 10 pages (200 comments)
-for(let i = 0; i < 10; i++){
-    ytcomments.scrape_next_page_youtube_comments(videoId).then((data) =>{
-        console.log(data);
-    }).catch((error)=>{
-        console.log(error);
-    });
+const payload = {
+  videoId: videoId, // Required
+  sortByNewest: sortByNewest,
+  setCookie: setCookie,
+  xsrf: xsrf,
+  continuation: continuation
 }
-//required when a new video is being watched. This clears all data that is required being able to load one page per function call
-ytcomments.cleanupStatics()
+
+ytcm.getComments(payload).then((data) =>{
+    console.log(data);
+}).catch((error)=>{
+    console.log(error);
+});
 ```
 **Returned Data**
 
 The data is returned as a list of objects (seen below). The replies have the same structure, except they are missing the replies attribute.
-Everything is a string because the given format is string and then everyone can do what they want with the data without converting it back to string format.
 ```javascript
 // The data is a list of objects containing the following attributes:
 {
--- src/Youtube-Scraper.js
+++ src/Youtube-Scraper.js
@@ -1,255 +1,95 @@
 const HttpRequester = require("./HttpRequester")
-//const fs = require('fs')
-const  html2json = require('html2json');
+const htmlParser = require('./htmlParser')
 
 class CommentScraper {
+    static async getComments(payload) {
+      if (typeof payload.videoId === 'undefined') {
+        return Promise.reject('No video Id given')
+      }
 
-    constructor(setCookie = true, sortNewest = false) {
-      this.XSRF_TOKEN = null
-      this.PAGE_TOKEN = 'FillToken'
-      this.FIRST_PAGE = true
-      this.requester = new HttpRequester(setCookie, sortNewest)
-    }
-    //starting point
-    async scrape_all_youtube_comments(videoId) {
-        const request_data = await this.requester.requestVideoPage(videoId);
-        const data = await this.parse_html(request_data.data, videoId);
-        this.requester.deleteSession()
-        return data
-    }
-    async scrape_next_page_youtube_comments(videoId) {
-        let request_data = {
-            data: null
-        }
-        if (this.FIRST_PAGE) {
-            request_data = await this.requester.requestVideoPage(videoId);
-            this.FIRST_PAGE = false
-        }
-        const data = await this.parse_next_html(request_data.data, videoId);
-        return data
-    }
+      let setCookie = true
 
-    async parse_next_html(html_data=null, videoId) {
-        let pre_token = null
-        //first iteration doesnt have a page token
-        let first_iteration = false
-        if (html_data !== null){
-            pre_token = html_data.match(/"XSRF_TOKEN":"[^"]*"/)[0]
-            first_iteration = true
-            // token embedded in page, needed for ajax request
-            this.XSRF_TOKEN = pre_token.substring(14, pre_token.length-1)
-            this.XSRF_TOKEN = this.XSRF_TOKEN.replace(/\\u003d/g, "=")
+      if (typeof (payload.setCookie) !== 'undefined') {
+        setCookie = payload.setCookie
+      }
 
-        }
-        let comments = []
-        const data = {
-             video_id: videoId,
-            session_token: this.XSRF_TOKEN
-        }
-        const params = {
-            action_load_comments: 1,
-            order_by_time: true,
-            filter: videoId,
-            order_menu: false
-        }
-        if (first_iteration) {
-            params.order_menu = true
-        } else {
-            if (this.PAGE_TOKEN === "") {
-                return null
-            }
-            data.page_token = this.PAGE_TOKEN
-        }
-        const ajaxResponse = await this.requester.ajax_request(data, params)
-        if (ajaxResponse === undefined) {
-            return
-        }
-        this.PAGE_TOKEN = ajaxResponse.data.page_token
-        const ajaxHtml = ajaxResponse.data.html_content
-        comments = comments.concat(this.extractCommentHtmlEntries(ajaxHtml))
-        return comments
+      let xsrf
+      let sort = 'newest'
+      const requester = new HttpRequester(false, true)
+
+      if (typeof payload.xsrf === 'undefined') {
+        xsrf = await requester.getXsrfToken(payload.videoId, setCookie)
+      } else {
+        xsrf = payload.xsrf
+      }
+
+      if (payload.sort !== 'undefined') {
+        sort = payload.sort
+      }
+
+      const commentsPayload = {
+        videoId: payload.videoId,
+        session_token: xsrf,
+        order_by_time: payload.sortByNewest,
+        filter: payload.videoId,
+        order_menu: payload.continuation ? false : true,
+        page_token: payload.continuation ? payload.continuation : undefined
+      }
+
+      const commentPageResponse = await requester.requestCommentsPage(commentsPayload, true)
+      let continuation = commentPageResponse.data.page_token
+      const commentHtml = commentPageResponse.data.html_content
+      const commentData = htmlParser.parseHtmlData(commentHtml)
+
+      if (continuation === '') {
+        continuation = null
+      }
+
+      return {
+        comments: commentData,
+        continuation: continuation,
+        xsrf: xsrf
+      }
     }
 
-    //extract the required data from the initial page and then all successive pages
-    async parse_html(html_data, videoId) {
+    static async getAllComments(videoId, sortByNewest=false, setAgent=true) {
+      this.continueGrabbingComments = true
 
-        const pre_token = html_data.match(/"XSRF_TOKEN":"[^"]*"/)[0]
-        // token embedded in page, needed for ajax request
-        const XSRF_TOKEN = pre_token.substr(14, pre_token.length - 15)
-        let comments = []
-        //first iteration doesnt have a page token
-        let first_iteration = true
-        let pageToken = "FillToken"
-        while (pageToken !== "") {
-            const data = {
-                video_id: videoId,
-                session_token: XSRF_TOKEN
-            }
-            const params = {
-                action_load_comments: 1,
-                order_by_time: true,
-                filter: videoId,
-                order_menu: false
-            }
-            if (first_iteration) {
-                params.order_menu = true
-            } else {
-                data.page_token = pageToken
-            }
-            const ajaxResponse = await this.requester.ajax_request(data, params)
-            if (ajaxResponse === undefined) {
-                return
-            }
-            pageToken = ajaxResponse.data.page_token
-            const ajaxHtml = ajaxResponse.data.html_content
-            //const commentIds = this.extractCommentIds(ajaxHtml)
-            comments = comments.concat(this.extractCommentHtmlEntries(ajaxHtml))
-            first_iteration = false
-        }
-        return comments
+      let comments = []
+
+      let payload = {
+        videoId: videoId,
+        setAgent: setAgent,
+        sortByNewest: sortByNewest
+      }
+
+      let commentResponse = await this.getComments(payload)
+      console.log(commentResponse)
+      comments = comments.concat(commentResponse.comments)
+
+      console.log(comments)
+      console.log(commentResponse.comments)
+
+      while (commentResponse.continuation !== null && this.continueGrabbingComments) {
+        payload.xsrf = commentResponse.xsrf
+        payload.continuation = commentResponse.continuation
+        commentResponse = await this.getComments(payload)
+        comments = comments.concat(commentResponse.comments)
+      }
+
+      if (!this.continueGrabbingComments) {
+        return Promise.reject('Process ended early')
+      }
+
+      return {
+        comments: comments,
+        xsrf: null,
+        continuation: null
+      }
     }
 
-    extractCommentIds(html_data) {
-        const commentIdsDouble = html_data.match(/data-cid="[^"]*/g)
-        const commentIdsSingle = []
-        // because we have every id two times, we can delete one of each kind
-        commentIdsDouble.forEach((value, index) => {
-            if (index % 2 === 0) {
-                commentIdsSingle.push(value.substr(10))
-            }
-        })
-        return commentIdsSingle
-    }
-
-    extractCommentHtmlEntries(html_data) {
-        const jsondata = html2json.html2json(html_data)
-        const comments = []
-        for(let i = 1; i < jsondata.child.length; i+=2) {
-            try {
-                const commentEntry = jsondata.child[i]
-                const comment = {
-                    id: commentEntry.child[1].attr["data-cid"],
-                    authorThumb: commentEntry.child[1].child[1].child[1].attr.src,
-                    author: commentEntry.child[1].child[3].child[1].child[1].child[0].text,
-                    authorLink: commentEntry.child[1].child[3].child[1].child[1].attr.href,
-                    text: this.buildText(commentEntry.child[1].child[3].child[3].child[1].child),
-                    likes: 0,
-                    isHearted: commentEntry.child[1].child[3].child[5].child[1].child.find(c => c.tag === 'table').child[1].child[5].child[1].attr.hasOwnProperty('data-action-on'),
-                    replies: this.extractCommentRepliesFromJSON(commentEntry)
-                }
-                if (commentEntry.child[1].child[3].child[5].child[1].child[5].attr.class[1] === 'off') {
-                    comment.likes = commentEntry.child[1].child[3].child[5].child[1].child[5].child[0].text
-                }
-                const maxLength = commentEntry.child[1].child[3].child[1].child.length
-                for (let j = 3; j < maxLength; j++) {
-                    if (commentEntry.child[1].child[3].child[1].child[j].node === "element" && commentEntry.child[1].child[3].child[1].child[j].tag === "a") {
-                        comment.time = this.extractTimeStringFromWhiteSpace(commentEntry.child[1].child[3].child[1].child[j].child[0].text)
-                        comment.edited = commentEntry.child[1].child[3].child[1].child[j].child[0].text.includes('edited');
-                    }
-                }
-                comment.numReplies = comment.replies.length
-                comment.hasReplies = (comment.numReplies > 0)
-                comments.push(comment)
-            } catch (error) {
-                console.error(error)
-                continue
-            }
-
-        }
-        return comments
-    }
-
-    buildText(textArray) {
-        let wholeComment = ""
-        for (let i = 0; i < textArray.length; i++) {
-            if (textArray[i].node === "text") {
-                //normal text
-                wholeComment += textArray[i].text
-            } else if (textArray[i].node === "element") {
-                if (textArray[i].tag === "a") {
-                    // links
-                    wholeComment += textArray[i].child[0].text
-                } else if (['b', 'i', 's'].includes(textArray[i].tag)) {
-                    // emphasis tags
-                    wholeComment += this.extractTextFromEmphasisTags(textArray[i])
-                }
-            }
-        }
-        return wholeComment
-    }
-
-    extractTextFromEmphasisTags(textElement) {
-        let result = {...textElement};
-        if (result.tag === "b") {
-            result = result.child[0];
-        }
-
-        if (result.tag === "i") {
-            result = result.child[0];
-        }
-
-        if (result.tag === "s") {
-            result = result.child[0].child[0];
-        }
-
-        return result.text;
-    }
-
-    extractCommentRepliesFromJSON(parentComment) {
-        const replies = []
-        if (!(parentComment.child[3].child.length > 1)){
-            return []
-        }
-        for(let i = 1; i < parentComment.child[3].child.length; i+=2){
-            try {
-                const commentEntry = parentComment.child[3].child[i]
-                const comment = {
-                    authorLink: commentEntry.child[1].attr.href,
-                    authorThumb: commentEntry.child[1].child[1].attr.src,
-                    author: commentEntry.child[3].child[1].child[1].child[0].text,
-                    text: this.buildText(commentEntry.child[3].child[3].child[1].child),
-                    id: commentEntry.attr["data-cid"]
-                }
-                const maxLength = commentEntry.child[3].child[1].child
-                for (let j = 2; j < maxLength; j++) {
-                    if (commentEntry.child[3].child[1].child[j].node === "element" && commentEntry.child[3].child[1].child[j].tag === "a") {
-                        comment.time = this.extractTimeStringFromWhiteSpace(commentEntry.child[3].child[1].child[j].child[0].text)
-                        if (commentEntry.child[3].child[1].child[j].child[0].text.includes('edited')) {
-                            comment.edited = true
-                        }
-                    }
-                }
-                replies.push(comment)
-            } catch (error) {
-                console.error(error)
-                continue
-            }
-        }
-        return replies
-    }
-
-    extractTimeStringFromWhiteSpace(originalString){
-        let timeAmount = originalString.match(/\d+/)[0]
-        let measurements = timeAmount === "1" ?
-			["second", "minute", "hour", "day", "week", "month", "year"] :
-			["seconds", "minutes", "hours", "days", "weeks", "months", "years"]
-
-
-        for (let measurement of measurements) {
-            if (originalString.includes(measurement)) {
-                return `${timeAmount} ${measurement}`
-            }
-        }
-
-        // This code should never run under normal circunstances
-        return "Unknown";
-    }
-
-    cleanupStatics(){
-        this.PAGE_TOKEN = "FillToken"
-        this.FIRST_PAGE = true
-        this.XSRF_TOKEN = null
-        this.requester.deleteSession()
+    static stopGetAllComments() {
+      this.continueGrabbingComments = false
     }
 }
 
