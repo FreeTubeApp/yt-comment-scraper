fa608ec8e8f63b97080843ad1c8be67a8fd8d5c0
Christian <24399312+chdzma@users.noreply.github.com>
2021-06-15 18:40:57 +0200

    Added option to add https agent

--- src/HttpRequester.js
+++ src/HttpRequester.js
@@ -8,7 +8,7 @@ function random(start, end) {
 }
 
 class HttpRequester {
-    constructor(setCookie = false, proxyData = null) {
+    constructor(setCookie = false, httpsAgent = null) {
       this.setCookie = setCookie
       this.session = axios.create({
         baseURL: baseURL,
@@ -17,18 +17,9 @@ class HttpRequester {
           'X-YouTube-Client-Name': '1',
           'X-YouTube-Client-Version': '2.20210331.06.00',
           'accept-language': 'en-US,en;q=0.5',
-        }
+        },
+        httpsAgent: httpsAgent
       })
-      if (proxyData) {
-        this.session.defaults["proxy"] = {};
-        this.session.defaults.proxy.host = proxyData.host;
-        this.session.defaults.proxy.port = proxyData.port;
-        this.session.defaults.proxy.protocol = proxyData.protocol;
-        if (proxyData.auth) {
-          this.session.defaults.auth.password = proxyData.auth.username;
-          this.session.defaults.auth.username = proxyData.auth.password;
-        }
-      }
       // NOTE: This currently provides a CONSENT cookie to
       // everyone, including non-European populations,
       // making this cookie potentially fingerprintable
