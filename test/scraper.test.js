d1ddd12b67a3d5c183e4a849499feb7d5345cad8
Svallinn <41585298+Svallinn@users.noreply.github.com>
2021-07-16 22:03:20 +0100

    Adapt tests to new source code

--- test/scraper.test.js
+++ test/scraper.test.js
@@ -5,23 +5,23 @@ describe('Standalone Mode: Comment Testing', () => {
     console.log("Please be advised that these tests only cover the Standalone mode of the module.\nThe Integration mode with applications like Electron cannot be tested without including such tools.\nUnder normal circumstances Integration mode should work, if comments on FreeTube work.")
 
     test('Scrape top video comments of first page', () => {
-        const parameters = {videoId: 'oBLQmE-nG60', setCookie: true, sortByNewest: false};
+        const parameters = {videoId: 'oBLQmE-nG60', mustSetCookie: true, sortByNewest: false};
         return ytcm.getComments(parameters).then((data) => {
-            expect(data.comments).toHaveLength(20);
+            expect(data.comments).not.toHaveLength(0);
         });
     });
 
     test('Scrape newest video comments of first page', () => {
-        const parameters = {videoId: 'oBLQmE-nG60', setCookie: true, sortByNewest: true};
+        const parameters = {videoId: 'oBLQmE-nG60', mustSetCookie: true, sortByNewest: true};
         return ytcm.getComments(parameters).then((data) => {
-            expect(data.comments).toHaveLength(20);
+            expect(data.comments).not.toHaveLength(0);
         });
     });
 
     test('Scrape newest video comments second page', () => {
-        let parameters = {videoId: 'oBLQmE-nG60', setCookie: true, sortByNewest: true, continuation: null};
+        let parameters = {videoId: 'oBLQmE-nG60', mustSetCookie: true, sortByNewest: true, continuation: null};
         return ytcm.getComments(parameters).then((data) => {
-            parameters["continuation"] = data.continuation;
+            parameters.continuation = data.continuation;
             ytcm.getComments(parameters).then((data) => {
                 expect(data.comments).not.toHaveLength(0);
             });
@@ -29,15 +29,15 @@ describe('Standalone Mode: Comment Testing', () => {
     });
 
     test('Scrape top replies of first page', () => {
-        const parameters = {videoId: 'oBLQmE-nG60', setCookie: true, sortByNewest: false};
+        const parameters = {videoId: 'oBLQmE-nG60', mustSetCookie: true, sortByNewest: false};
         // This test first gets comments of the video with above Id
         return ytcm.getComments(parameters).then((data) => {
             for (let i = 0; i < data.comments.length; i++) {
                 // The test searches for a comment which does have at least 1 reply and then ask for the replies
                 if (data.comments[i].numReplies > 0) {
-                    const replyParameters = {videoId: 'oBLQmE-nG60', replyToken: data.comments[i].replyToken, setCookie: true};
+                    const replyParameters = {videoId: 'oBLQmE-nG60', replyToken: data.comments[i].replyToken, mustSetCookie: true};
                     return ytcm.getCommentReplies(replyParameters).then((replyData) => {
-                        expect(replyData.comments).toHaveLength(10);
+                        expect(replyData.comments).not.toHaveLength(0);
                     });
                 }
             }
@@ -45,13 +45,13 @@ describe('Standalone Mode: Comment Testing', () => {
     });
 
     test('Scrape second batch of top replies of first page', () => {
-        const parameters = {videoId: 'oBLQmE-nG60', setCookie: true, sortByNewest: false};
+        const parameters = {videoId: 'oBLQmE-nG60', mustSetCookie: true, sortByNewest: false};
         // This test first gets comments of the video with above Id
         return ytcm.getComments(parameters).then((data) => {
             for (let i = 0; i < data.comments.length; i++) {
                 // The test searches for a comment which does have at least 1 reply and then ask for the replies
                 if (data.comments[i].numReplies > 0) {
-                    let replyParameters = {videoId: 'oBLQmE-nG60', replyToken: data.comments[i].replyToken, setCookie: true};
+                    let replyParameters = {videoId: 'oBLQmE-nG60', replyToken: data.comments[i].replyToken, mustSetCookie: true};
                     return ytcm.getCommentReplies(replyParameters).then((replyData) => {
                         replyParameters.replyToken = replyData.continuation;
                         return ytcm.getCommentReplies(replyParameters).then((replyData) => {
@@ -64,7 +64,7 @@ describe('Standalone Mode: Comment Testing', () => {
     });
 
     test('Scrape video without comments', () => {
-        const parameters = {videoId: 'Bj-3M-KqZsI', setCookie: true, sortByNewest: false};
+        const parameters = {videoId: 'Bj-3M-KqZsI', mustSetCookie: true, sortByNewest: false};
         return ytcm.getComments(parameters).then((data) => {
             expect(data.comments).toHaveLength(0);
         });
